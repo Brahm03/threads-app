a8236453ddd60f88879c64c4be8da239cdfc83a7
AJmal <96195451+Brahm03@users.noreply.github.com>
2026-03-24 19:43:29 +0500

    firebase connected

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:threads_app/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding
+      .ensureInitialized(); // * native code (android/ios) start
+  await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform); // * firebase -> flutter
   runApp(const MainApp());
 }
 
